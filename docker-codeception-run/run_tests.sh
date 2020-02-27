9b9fbccbc8e41b0ab3bed480b6675093d80afdcb
BSCheshir <bscheshir@mail.ru>
2020-02-27 06:12:17 +0300

    Update run_tests.sh

--- docker-codeception-run/run_tests.sh
+++ docker-codeception-run/run_tests.sh
@@ -2,6 +2,7 @@
 cd /var/www/html
 composer show
 composer global show
+composer require codeception/module-filesystem codeception/module-yii2 --dev
 ls -luha
 #todo: use wait-fot-it
 #./yii migrate/up --interactive=0
