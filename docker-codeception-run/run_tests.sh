c630ea0fdacee3db4d50f9ce2217a6b70621e1f8
BSCheshir <bscheshir@mail.ru>
2020-02-29 03:36:39 +0300

    Update run_tests.sh

--- docker-codeception-run/run_tests.sh
+++ docker-codeception-run/run_tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-cd /var/www/html
-composer show
+echo "composer global show"
 composer global show
+cd /repo
+echo "codeception: composer show"
+composer show
+cd /var/www/html
+echo "testing code: composer show"
+composer show
 ls -luha
 ./yii migrate/up --interactive=0
 codecept run --no-redirect
